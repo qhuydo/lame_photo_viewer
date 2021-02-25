13f8e6716605b68dac9a09eabd53caf47b787813
qhuydo <q.huy.do.172@gmail.com>
2021-02-25 22:05:53 +0700

    Add and setup Timber
    [+] build.gradle, app/build.gradle: add Timber dependency.
    [+] PhotosActivity.kt: add the application class that add Timber
        instance initialization.
    [*] AndroidManifest.xml: add android:name attribute to the application
       tag in order to use the PhotosActivity class.

---
+++ Photos/app/src/main/java/com/hcmus/clc18se/photos/PhotosApplication.kt
@@ -0,0 +1,29 @@
+package com.hcmus.clc18se.photos
+
+import android.app.Application
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
+/**
+ * Override application to setup Timber log instance
+ */
+@Suppress("unused")
+class PhotosApplication: Application() {
+
+    private val applicationScope = CoroutineScope(Dispatchers.Default)
+
+    override fun onCreate() {
+        super.onCreate()
+        delayInit()
+    }
+
+    fun delayInit() {
+
+        // Init Timber instance
+        applicationScope.launch {
+            Timber.plant(Timber.DebugTree())
+        }
+    }
+}
