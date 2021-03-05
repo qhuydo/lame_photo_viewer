7bf8363b11fd69d10d47e0c9f7fe78a853b06551
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2021-03-05 10:41:47 +0700

    Thêm trang edit bright, color, xóa những icon không có outline
    xóa icon không có outline để tạo thuận lợi cho việc đổi màu của đồ án

--- Photos/app/src/main/java/com/hcmus/clc18se/photos/EditPhotoActivity.kt
+++ Photos/app/src/main/java/com/hcmus/clc18se/photos/EditPhotoActivity.kt
@@ -1,4 +1,32 @@
 package com.hcmus.clc18se.photos
 
-class EditPhotoActivity {
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import android.view.MenuItem
+import timber.log.Timber
+
+class EditPhotoActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_edit_photo)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Handle item selection
+        when (item.getItemId()) {
+            R.id.bright -> {
+            }
+            R.id.add_icon -> {
+            }
+            R.id.draw -> {
+            }
+            R.id.crop -> {
+            }
+            R.id.filter -> {
+            }
+            R.id.changeColor -> {
+            }
+            else -> Timber.w("No edit option has been set")
+        }
+    }
 }
