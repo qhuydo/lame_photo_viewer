576d82db813e3550b7ab27bb622a1c9afeea0adc
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2021-03-08 11:35:04 +0700

    Add configuration bar when click navigation button from EditPhotsActivity
    them activity edit photo vao manifest
    them action click navigation view se hien ra tuong ung cua navigation do o edit photo

--- Photos/app/src/main/java/com/hcmus/clc18se/photos/EditPhotoActivity.kt
+++ Photos/app/src/main/java/com/hcmus/clc18se/photos/EditPhotoActivity.kt
@@ -1,41 +1,52 @@
 package com.hcmus.clc18se.photos
 
 import android.os.Bundle
-import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import android.view.MenuItem
 import android.view.View
-import android.widget.Button
 import com.google.android.material.bottomnavigation.BottomNavigationView
-import com.hcmus.clc18se.photos.databinding.ActivityPhotosBinding
+import com.hcmus.clc18se.photos.databinding.ActivityEditPhotoBinding
 import timber.log.Timber
 
 class EditPhotoActivity : AppCompatActivity() {
-    var choose = 0
-    private val binding by lazy { ActivityPhotosBinding.inflate(layoutInflater) }
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
-        setContentView(R.layout.activity_edit_photo)
-        val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_edit)
+    private val binding by lazy { ActivityEditPhotoBinding.inflate(layoutInflater) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Timber.d("On create")
+        setContentView(binding.root)
+        Timber.d("On created")
+        val bottomNavigation = binding.bottomEdit
+        bottomNavigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        setBarVisibility(R.id.bright)
     }
-    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.filter_editor -> {
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.bright_editor -> {
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.color_editor -> {
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.add_icon_editor -> {
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.crop_editor -> {
-                return@OnNavigationItemSelectedListener true
-            }
+
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        if (item.itemId in listOf(R.id.bright,
+                        R.id.filter,
+                        R.id.add_icon,
+                        R.id.crop,
+                        R.id.change_color)) {
+            setBarVisibility(item.itemId)
+            return@OnNavigationItemSelectedListener true
         }
         false
     }
+
+    private fun setBarVisibility(itemId: Int) {
+        binding.apply {
+            brightEditor.visibility = View.GONE
+            filterEditor.visibility = View.GONE
+            addIconEditor.visibility = View.GONE
+            cropEditor.visibility = View.GONE
+            colorEditor.visibility = View.GONE
+        }
+        when (itemId) {
+            R.id.bright -> binding.brightEditor.visibility = View.VISIBLE
+            R.id.filter -> binding.filterEditor.visibility = View.VISIBLE
+            R.id.add_icon -> binding.addIconEditor.visibility = View.VISIBLE
+            R.id.crop -> binding.cropEditor.visibility = View.VISIBLE
+            R.id.change_color -> binding.colorEditor.visibility = View.VISIBLE
+        }
+    }
+
 }
