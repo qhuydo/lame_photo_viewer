b9d484db38214be32259a1a1849ecf3440a641fe
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2021-03-10 10:30:44 +0700

    change value of xml file to value dimens.xml, add save state edit photo
    add save state edit photo to fix bug when rotate screen in phone
    add value dimens.xml

--- Photos/app/src/main/java/com/hcmus/clc18se/photos/EditPhotoActivity.kt
+++ Photos/app/src/main/java/com/hcmus/clc18se/photos/EditPhotoActivity.kt
@@ -1,23 +1,40 @@
 package com.hcmus.clc18se.photos
 
+import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.hcmus.clc18se.photos.databinding.ActivityEditPhotoBinding
 import timber.log.Timber
 
 class EditPhotoActivity : AppCompatActivity() {
     private val binding by lazy { ActivityEditPhotoBinding.inflate(layoutInflater) }
+    private var cur_item_id = 0
+    private val bottomAppBarItemKey: String = "curItemId"
+    private var BitmapImage: Bitmap? = null
+    private var imageView: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Timber.d("On create")
         setContentView(binding.root)
-        Timber.d("On created")
+
+        val cur_intent = intent
+        val myBundle = cur_intent.extras
+        BitmapImage = myBundle!!.getParcelable<Bitmap>("image")
+        imageView = binding.imageEdit
+        imageView?.setImageBitmap(BitmapImage)
+
         val bottomNavigation = binding.bottomEdit
         bottomNavigation.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
         setBarVisibility(R.id.bright)
+        savedInstanceState?.let {
+            cur_item_id = it.getInt(bottomAppBarItemKey)
+            setBarVisibility(cur_item_id)
+        }
     }
 
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
@@ -27,9 +44,15 @@ class EditPhotoActivity : AppCompatActivity() {
                         R.id.crop,
                         R.id.change_color)) {
             setBarVisibility(item.itemId)
+            cur_item_id = item.itemId
             return@OnNavigationItemSelectedListener true
         }
         false
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(bottomAppBarItemKey,cur_item_id)
     }
 
     private fun setBarVisibility(itemId: Int) {
@@ -39,6 +62,7 @@ class EditPhotoActivity : AppCompatActivity() {
             addIconEditor.visibility = View.GONE
             cropEditor.visibility = View.GONE
             colorEditor.visibility = View.GONE
+            drawEditor.visibility = View.GONE
         }
         when (itemId) {
             R.id.bright -> binding.brightEditor.visibility = View.VISIBLE
@@ -46,7 +70,16 @@ class EditPhotoActivity : AppCompatActivity() {
             R.id.add_icon -> binding.addIconEditor.visibility = View.VISIBLE
             R.id.crop -> binding.cropEditor.visibility = View.VISIBLE
             R.id.change_color -> binding.colorEditor.visibility = View.VISIBLE
+            R.id.draw -> binding.drawEditor.visibility = View.VISIBLE
         }
+    }
+
+    fun onDrawButtonClick(view: View){
+        setBarVisibility(R.id.draw)
+    }
+
+    fun onSaveImageButtonClick(view: View){
+
     }
 
 }
