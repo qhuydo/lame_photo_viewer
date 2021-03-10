8e406166ef1b4afdfba7033e43a0ed8af0bad3de
qhuydo <q.huy.do.172@gmail.com>
2021-03-10 23:24:00 +0700

    Add toolbar menu and bottom bar bottom icons for fragment_photo_view
    [+] view_photo_menu.xml: menu chi tiết của toolbar.
    [+] photo_view_bottom.xml: các button icons dưới cùng.

--- Photos/app/src/main/java/com/hcmus/clc18se/photos/fragments/PhotoViewPagerFragment.kt
+++ Photos/app/src/main/java/com/hcmus/clc18se/photos/fragments/PhotoViewPagerFragment.kt
@@ -1,11 +1,11 @@
 package com.hcmus.clc18se.photos.fragments
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.app.ActionBar
 import androidx.fragment.app.Fragment
 import com.davemorrissey.labs.subscaleview.ImageSource
+import com.hcmus.clc18se.photos.PhotosActivity
 import com.hcmus.clc18se.photos.R
 import com.hcmus.clc18se.photos.databinding.PhotoViewPagerPageBinding
 
@@ -14,6 +14,7 @@ class PhotoViewPagerFragment : Fragment() {
     internal var resId: Int = R.drawable.ic_launcher_sample
 
     private lateinit var binding: PhotoViewPagerPageBinding
+    private var actionBar: ActionBar? = null
 
     override fun onCreateView(
             inflater: LayoutInflater,
@@ -21,16 +22,39 @@ class PhotoViewPagerFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         binding = PhotoViewPagerPageBinding.inflate(inflater, container, false)
+        setHasOptionsMenu(true)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        actionBar = (activity as PhotosActivity).supportActionBar
+
         if (savedInstanceState?.containsKey(BUNDLE_RESID) == true) {
             resId = savedInstanceState.getInt(BUNDLE_RESID)
         }
+
         binding.imageView.setImage(ImageSource.resource(resId))
 
+        binding.imageView.setOnClickListener {
+            actionBar?.let {
+                if (it.isShowing) {
+                    it.hide()
+                } else {
+                    it.show()
+                }
+            }
+        }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.view_photo_menu, menu)
+    }
+
+    override fun onDetach() {
+        actionBar?.show()
+        super.onDetach()
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
