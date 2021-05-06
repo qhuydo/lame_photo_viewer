591170dc0825988d43a5cbb60ed4095fde9b6d5b
HaTheHien <47638822+HaTheHien@users.noreply.github.com>
2021-05-06 09:24:30 +0700

    add function dectect face from list media item
    [+] function get face from list media item but not test

--- Photos/app/src/main/java/com/hcmus/clc18se/photos/fragments/PeopleFragment.kt
+++ Photos/app/src/main/java/com/hcmus/clc18se/photos/fragments/PeopleFragment.kt
@@ -1,16 +1,24 @@
 package com.hcmus.clc18se.photos.fragments
 
+import android.graphics.Bitmap
+import android.media.FaceDetector
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.Toolbar
+import androidx.core.graphics.scale
 import androidx.databinding.DataBindingUtil
 import com.google.android.material.appbar.AppBarLayout
 import com.google.android.material.transition.MaterialSharedAxis
 import com.hcmus.clc18se.photos.R
+import com.hcmus.clc18se.photos.adapters.bindImage
+import com.hcmus.clc18se.photos.data.MediaItem
 import com.hcmus.clc18se.photos.databinding.FragmentPeopleBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class PeopleFragment : BaseFragment() {
 
@@ -40,4 +48,28 @@ class PeopleFragment : BaseFragment() {
     override fun getAppbar(): AppBarLayout = binding.topAppBar.appBarLayout
 
     override fun getToolbarTitleRes(): Int = R.string.people_title
+
+    private fun listFaceImage(list:List<MediaItem>):List<MediaItem>{
+        val newList = ArrayList<MediaItem>()
+        for (item in list){
+            var bitmap: Bitmap = MediaStore.Images.Media.getBitmap(requireContext().contentResolver, item.requireUri())
+            var scale = 1
+            val byteBitmap = bitmap.width * bitmap.height * 4
+            while (byteBitmap / scale / scale > 6000000) {
+                scale++
+            }
+
+            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true).scale(
+                    bitmap.width / scale,
+                    bitmap.height / scale,
+                    false
+            )
+            val faceDetector:FaceDetector = FaceDetector(bitmap.width,bitmap.height,1)
+            val faces = Array<FaceDetector.Face?>(1){null}
+            val numFace = faceDetector.findFaces(bitmap, faces)
+            if (numFace > 0)
+                newList.add(item)
+        }
+        return newList
+    }
 }
