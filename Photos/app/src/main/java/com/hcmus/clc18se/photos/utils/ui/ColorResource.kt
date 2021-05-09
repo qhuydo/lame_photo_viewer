db6e4092eea3dfb6befb5083639152d4f5382971
qhuydo <46428103+qhuydo@users.noreply.github.com>
2021-05-09 13:59:59 +0700

    Change the color picker dialog in `EditPhotoActivity`
    [*] arrays.xml: thêm mảng palette màu từ colors.xml.
    [*] ColorResource.kt: thêm hàm lấy palette màu từ arrays.xml.
    [*] EditPhotoActivity.kt: sử dụng MaterialDialog thay thế cho thư viện
        ColorDialog.

--- Photos/app/src/main/java/com/hcmus/clc18se/photos/utils/ui/ColorResource.kt
+++ Photos/app/src/main/java/com/hcmus/clc18se/photos/utils/ui/ColorResource.kt
@@ -1,19 +1,24 @@
 package com.hcmus.clc18se.photos.utils.ui
 
 import android.app.Application
+import android.content.Context
 import android.content.SharedPreferences
 import android.content.pm.PackageManager
 import android.content.res.Configuration
+import android.content.res.TypedArray
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.res.use
 import com.hcmus.clc18se.photos.AbstractPhotosActivity
 import com.hcmus.clc18se.photos.R
 import timber.log.Timber
 
+
 class ColorResource(
-        private val application: Application,
-        private val preferences: SharedPreferences) {
+    private val application: Application,
+    private val preferences: SharedPreferences
+) {
 
     private val resources by lazy { application.resources }
 
@@ -24,38 +29,41 @@ class ColorResource(
 
     private val colorThemeMapper by lazy {
         listOf(
-                R.color.red_500 to R.style.Theme_Photos_Red_NoActionBar,
-                R.color.deep_orange_500 to R.style.Theme_Photos_Orange_NoActionBar,
-                R.color.amber_500 to R.style.Theme_Photos_Yellow_NoActionBar,
-                R.color.green_500 to R.style.Theme_Photos_Green_NoActionBar,
-                R.color.blue_500 to R.style.Theme_Photos_Blue_NoActionBar,
-                R.color.indigo_500 to R.style.Theme_Photos_Indigo_NoActionBar,
-                R.color.dark_purple_500 to R.style.Theme_Photos_Purple_NoActionBar,
-                R.color.pink_500 to R.style.Theme_Photos_Pink_NoActionBar,
-                R.color.brown_500 to R.style.Theme_Photos_Brown_NoActionBar,
-                R.color.grey_500 to R.style.Theme_Photos_Grey_NoActionBar
+            R.color.red_500 to R.style.Theme_Photos_Red_NoActionBar,
+            R.color.deep_orange_500 to R.style.Theme_Photos_Orange_NoActionBar,
+            R.color.amber_500 to R.style.Theme_Photos_Yellow_NoActionBar,
+            R.color.green_500 to R.style.Theme_Photos_Green_NoActionBar,
+            R.color.blue_500 to R.style.Theme_Photos_Blue_NoActionBar,
+            R.color.indigo_500 to R.style.Theme_Photos_Indigo_NoActionBar,
+            R.color.dark_purple_500 to R.style.Theme_Photos_Purple_NoActionBar,
+            R.color.pink_500 to R.style.Theme_Photos_Pink_NoActionBar,
+            R.color.brown_500 to R.style.Theme_Photos_Brown_NoActionBar,
+            R.color.grey_500 to R.style.Theme_Photos_Grey_NoActionBar
         ).map { resources.getInteger(it.first) to it.second }.toMap()
     }
 
     private val colorResources by lazy {
         listOf(
-                R.color.red_500 to ICON_COLOR.RED,
-                R.color.deep_orange_500 to ICON_COLOR.ORANGE,
-                R.color.amber_500 to ICON_COLOR.YELLOW,
-                R.color.green_500 to ICON_COLOR.GREEN,
-                R.color.blue_500 to ICON_COLOR.BLUE,
-                R.color.indigo_500 to ICON_COLOR.INDIGO,
-                R.color.dark_purple_500 to ICON_COLOR.PURPLE,
-                R.color.pink_500 to ICON_COLOR.PINK,
-                R.color.brown_500 to ICON_COLOR.BROWN,
-                R.color.grey_500 to ICON_COLOR.GREY,
+            R.color.red_500 to ICON_COLOR.RED,
+            R.color.deep_orange_500 to ICON_COLOR.ORANGE,
+            R.color.amber_500 to ICON_COLOR.YELLOW,
+            R.color.green_500 to ICON_COLOR.GREEN,
+            R.color.blue_500 to ICON_COLOR.BLUE,
+            R.color.indigo_500 to ICON_COLOR.INDIGO,
+            R.color.dark_purple_500 to ICON_COLOR.PURPLE,
+            R.color.pink_500 to ICON_COLOR.PINK,
+            R.color.brown_500 to ICON_COLOR.BROWN,
+            R.color.grey_500 to ICON_COLOR.GREY,
         ).map { resources.getInteger(it.first) to it.second }
     }
 
     internal val colorResourceMapper by lazy { colorResources.toMap() }
 
     fun getCurrentThemeColor(): Int {
-        val currentColor = preferences.getInt(application.getString(R.string.app_color_key), R.color.indigo_500)
+        val currentColor = preferences.getInt(
+            application.getString(R.string.app_color_key),
+            R.color.indigo_500
+        )
         return colorThemeMapper[currentColor] ?: R.style.Theme_Photos_Indigo_NoActionBar
     }
 
@@ -97,24 +105,52 @@ class ColorResource(
                 Timber.d("Config default theme: ${uiMode ?: resources.configuration.uiMode}")
                 configDefaultTheme(uiMode ?: resources.configuration.uiMode)
             }
-            options[AbstractPhotosActivity.THEME_WHITE] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            options[AbstractPhotosActivity.THEME_DARK] -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            options[AbstractPhotosActivity.THEME_WHITE] -> AppCompatDelegate.setDefaultNightMode(
+                AppCompatDelegate.MODE_NIGHT_NO
+            )
+            options[AbstractPhotosActivity.THEME_DARK] -> AppCompatDelegate.setDefaultNightMode(
+                AppCompatDelegate.MODE_NIGHT_YES
+            )
             else -> Timber.w("No theme has been set")
         }
     }
 
     internal fun updateIcon(packageManager: PackageManager) {
         if (setNewIcon) {
-            val newColor = preferences.getInt(application.getString(R.string.app_color_key), R.color.indigo_500)
+            val newColor = preferences.getInt(
+                application.getString(R.string.app_color_key),
+                R.color.indigo_500
+            )
             Timber.d("Color ${colorResourceMapper[newColor] ?: ICON_COLOR.INDIGO}")
-            setIcon(packageManager, colorResourceMapper[newColor]
-                    ?: ICON_COLOR.INDIGO)
+            setIcon(
+                packageManager, colorResourceMapper[newColor]
+                    ?: ICON_COLOR.INDIGO
+            )
         }
     }
 
 }
 
 fun isColorDark(color: Int): Boolean {
-    val darkness: Double = 1 - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(color)) / 255
+    val darkness: Double =
+        1 - (0.299 * Color.red(color) + 0.587 * Color.green(color) + 0.114 * Color.blue(
+            color
+        )) / 255
     return darkness >= 0.5
+}
+
+fun Context.allColorPaletteRes(): Array<IntArray> {
+    resources.obtainTypedArray(R.array.color_picker_dialog_sub_values).use { ta ->
+
+        val n = ta.length()
+        val array = mutableListOf<IntArray>()
+
+        for (i in 0 until n) {
+            val id = ta.getResourceId(i, 0)
+            if (id > 0) {
+                array += resources.getIntArray(id)
+            }
+        }
+        return array.toTypedArray()
+    }
 }
